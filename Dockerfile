4035b15fc5767b51ab64075b611421215ec1ec75
Rajdeep Bharati <rajdeep@macports.org>
2020-09-29 23:16:48 +0530

    Set up rev proxy

--- Dockerfile
+++ Dockerfile
@@ -2,33 +2,17 @@ FROM golang:1.15.2-buster
 LABEL maintainer="Rajdeep Bharati <rajdeep@buidllabs.io>"
 
 RUN apt-get update
-RUN apt-cache depends ffmpeg
-RUN apt-get install -y ffmpeg
-RUN ffmpeg -version
-RUN apt-get install -y sqlite3
 
-RUN mkdir /demuxapp
-WORKDIR /demuxapp
+RUN mkdir /demuxipfsrevproxy
+WORKDIR /demuxipfsrevproxy
 
 COPY go.mod go.sum ./
 RUN go mod download
 COPY . .
 RUN go build cmd/main.go
 
-RUN wget https://github.com/rajdeepbharati/go-livepeer/releases/download/v0.5.10-demux.1/livepeer -P ./livepeerPull/linux
-RUN chmod +x ./livepeerPull/linux/livepeer
-
-ENV IPFS_GATEWAY=${IPFS_GATEWAY}
-ENV IPFS_REV_PROXY_ADDR=${IPFS_REV_PROXY_ADDR}
-ENV LIVEPEER_COM_API_KEY=${LIVEPEER_COM_API_KEY}
-ENV LIVEPEER_PRICING_TOOL=${LIVEPEER_PRICING_TOOL}
-ENV PINATA_API_KEY=${PINATA_API_KEY}
-ENV PINATA_SECRET_KEY=${PINATA_SECRET_KEY}
-ENV POLL_INTERVAL=${POLL_INTERVAL}
 ENV PORT=${PORT}
-ENV POW_TOKEN=${POW_TOKEN}
-ENV POWERGATE_ADDR=${POWERGATE_ADDR}
-ENV TRUSTED_MINERS=${TRUSTED_MINERS}
+ENV POW_IPFS=${POW_IPFS}
 
 EXPOSE ${PORT}
 
--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,59 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os"
+	"strings"
 
-	"github.com/buidl-labs/Demux/dataservice"
-	"github.com/buidl-labs/Demux/server"
-	"github.com/buidl-labs/Demux/util"
+	"github.com/julienschmidt/httprouter"
+	"github.com/sirupsen/logrus"
 )
 
+func singleJoiningSlash(a, b string) string {
+	aslash := strings.HasSuffix(a, "/")
+	bslash := strings.HasPrefix(b, "/")
+	switch {
+	case aslash && bslash:
+		return a + b[1:]
+	case !aslash && !bslash:
+		return a + "/" + b
+	}
+	return a + b
+}
+
 func main() {
-	dataservice.InitDB()
-	go util.RunPoller()
-	server.StartServer(":" + os.Getenv("PORT"))
+	logrus.SetFormatter(&logrus.TextFormatter{})
+	logrus.SetOutput(os.Stdout)
+	logrus.SetLevel(logrus.InfoLevel)
+	logger := logrus.WithFields(logrus.Fields{
+		"service": "go-reverse-proxy",
+	})
+
+	router := httprouter.New()
+	origin, _ := url.Parse(os.Getenv("POW_IPFS"))
+	path := "/*catchall"
+
+	reverseProxy := httputil.NewSingleHostReverseProxy(origin)
+
+	reverseProxy.Director = func(req *http.Request) {
+		req.Header.Add("X-Forwarded-Host", req.Host)
+		req.Header.Add("X-Origin-Host", origin.Host)
+		req.URL.Scheme = origin.Scheme
+		req.URL.Host = origin.Host
+
+		wildcardIndex := strings.IndexAny(path, "*")
+		proxyPath := singleJoiningSlash(origin.Path, req.URL.Path[wildcardIndex:])
+		if strings.HasSuffix(proxyPath, "/") && len(proxyPath) > 1 {
+			proxyPath = proxyPath[:len(proxyPath)-1]
+		}
+		req.URL.Path = proxyPath
+	}
+
+	router.Handle("GET", path, func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		reverseProxy.ServeHTTP(w, r)
+	})
+
+	logger.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
 }
